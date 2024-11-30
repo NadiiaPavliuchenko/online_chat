744685263be713144c5208428e9c789849f6d0e2
NadiiaPavliuchenko <n.pavljuchenko@gmail.com>
2024-11-30 21:31:26 +0200

    created base structure, root styles and installed dependencies

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "../node_modules/modern-normalize/modern-normalize.css";
+import App from "./components/App/App";
+import { RootStyles } from "./styles/rootStyles";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
+    <RootStyles />
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
